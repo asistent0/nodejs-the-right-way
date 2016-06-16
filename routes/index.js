01c934f5004aa12b69e8f85d42acd07bb2cb39d2
asistent <asistent0@gmail.com>
2016-06-16 22:36:15 +0300

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
-var markdown = require( "markdown" ).markdown;
+var markdown = require("markdown").markdown;
 var fs = require('fs');
 var router = express.Router();
 
-
-var md = markdown.toHTML(fs.readFileSync('guides/index.md').toString('utf8'));
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', value: md });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Node.JS the Right way',
+        value: markdown.toHTML(fs.readFileSync('guides/index.md').toString('utf8'))
+    });
+});
+
+router.get('/:uid/:file?', function (req, res, next) {
+    var url = req.originalUrl;
+    res.render('index', {value: markdown.toHTML(fs.readFileSync('guides/' + url + '.md').toString('utf8'))});
 });
 
 module.exports = router;
